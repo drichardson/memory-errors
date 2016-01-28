42b70c9679829415408f7ea0af41f87f90e77c2f
Doug Richardson <dougie.richardson@gmail.com>
2016-01-28 13:15:39 -0800

    add mprotect

--- measure-errors.go
+++ measure-errors.go
@@ -12,9 +12,17 @@ const allocationSize = 8 * GB
 func main() {
 	memory := make([]byte, allocationSize)
 
+	// mlock prevents memory from being swapped to disk
 	err := syscall.Mlock(memory)
 	if err != nil {
 		log.Fatalln("Error locking memory.", err)
+	}
+
+	// mprotect PROT_READ makes the memory read only. Writes on memory[i] after the syscall
+	// result in a fault that terminates the program.
+	err = syscall.Mprotect(memory, syscall.PROT_READ)
+	if err != nil {
+		log.Fatalln("Error protecting memory.", err)
 	}
 
 	for {
